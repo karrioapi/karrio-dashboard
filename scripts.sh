e7d9c4b7dc78842d6fbdda7c406d9a032802e77a
DanH91 <danielkobina@gmail.com>
2020-03-27 17:34:09 -0400

    reset project structure

--- scripts.sh
+++ scripts.sh
@@ -1,37 +1,61 @@
 #!/usr/bin/env bash
 
+# Python virtual environment helpers
+
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BASE_DIR="${PWD##*/}"
+ENV_DIR=".venv"
 
-init() {
-    deactivate || true
-    rm -r $ROOT/venv || true
-    mkdir -p $ROOT/venv
-    python3 -m venv $ROOT/venv/$BASE_DIR &&
-    source $ROOT/venv/$BASE_DIR/bin/activate &&
-    pip install -r requirements.txt &&
-    pip install -r requirements-dev.txt
+activate_env() {
+  echo "Activate $BASE_DIR"
+  deactivate || true
+  # shellcheck source=src/script.sh
+  source "${ROOT:?}/$ENV_DIR/$BASE_DIR/bin/activate"
 }
 
+create_env() {
+    echo "create $BASE_DIR Python3 env"
+    deactivate || true
+    rm -rf "${ROOT:?}/$ENV_DIR" || true
+    mkdir -p "${ROOT:?}/$ENV_DIR"
+    python3 -m venv "${ROOT:?}/$ENV_DIR/$BASE_DIR" &&
+    activate_env &&
+    pip install --upgrade pip
+}
+
+init() {
+    create_env &&
+    pip install -r requirements.txt &&
+    pip install -r requirements.dev.txt
+}
+
+
+alias env:new=create_env
+alias env:on=activate_env
+alias env:reset=init
+
+
+# Project helpers
+
 run_server() {
-    (echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell) > /dev/null 2>&1;
+    (echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell);
     python manage.py makemigrations && python manage.py migrate && python manage.py runserver
 }
 
 run_prod() {
-    APP_STAGE="production"
+    export APP_STAGE="production"
     run_server
 }
 alias run:prod=run_prod
 
 run_dev() {
-    APP_STAGE="developement"
+    export APP_STAGE="developement"
     run_server
 }
 alias run:dev=run_dev
 
 run_container() {
-    APP_STAGE="containerized"
+    export APP_STAGE="containerized"
     docker-compose up "$@"
 }
 alias run:container=run_container
