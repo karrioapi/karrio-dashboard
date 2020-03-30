3acf422ab4632246ec3943174ca5a56ff24cbf54
DanH91 <danielkobina@gmail.com>
2020-03-30 15:54:55 -0400

    implement (non-tested) shipping and rating with datatypes clean ups

--- purpleserver/proxy/purpleserver/proxy/views/rating.py
+++ purpleserver/proxy/purpleserver/proxy/views/rating.py
@@ -1,8 +1,10 @@
+import logging
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from rest_framework import status
 from rest_framework.decorators import api_view, authentication_classes, permission_classes, throttle_classes
+from rest_framework.request import Request
 from rest_framework.response import Response
 from django.urls import path
 
@@ -11,25 +13,32 @@ from drf_yasg import openapi
 
 from purplship.core.utils.helpers import to_dict
 
-from purpleserver.core import serializers
-from purpleserver.core.gateway import fetch_rates
+from purpleserver.core.serializers import RateRequest, RateResponse
+from purpleserver.core.gateway import fetch_rates, get_carriers
 from purpleserver.proxy.router import router
+
+logger = logging.getLogger(__name__)
 
 
 @swagger_auto_schema(
     methods=['post'],
-    responses={200: serializers.CompleteRateResponse()},
-    request_body=serializers.RateRequest(),
+    responses={200: RateResponse()},
+    request_body=RateRequest(),
     operation_description="""
-    POST /v1/rating?carrier=[carrier_type/carrier_name]
+    POST /v1/rating?carrier=[carrier]
     """,
     operation_id="ShippingRates",
     manual_parameter=[
         openapi.Parameter(
             'carrier',
             openapi.IN_QUERY,
-            required=False,
-            description="specific shipping carrier",
+            description="specific shipping carrier type",
+            type=openapi.TYPE_STRING
+        ),
+        openapi.Parameter(
+            'carrier_name',
+            openapi.IN_QUERY,
+            description="shipment name",
             type=openapi.TYPE_STRING
         )
     ]
@@ -38,17 +47,31 @@ from purpleserver.proxy.router import router
 @authentication_classes((SessionAuthentication, BasicAuthentication, TokenAuthentication))
 @permission_classes((IsAuthenticated, ))
 @throttle_classes([UserRateThrottle, AnonRateThrottle])
-def rate(request):
-    error = None
+def rate(request: Request):
     try:
-        if error is None:
-            return Response(
-                to_dict({}),
-                status=status.HTTP_200_OK
+        try:
+            query = request.query_params
+            carrier_settings_list = get_carriers(
+                carrier_type=query.get('carrier'),
+                carrier_name=query.get('carrier_name'),
             )
-        else:
-            return Response(error, status=status.HTTP_400_BAD_REQUEST)
+            request = RateRequest(data=request.data)
+
+            if len(carrier_settings_list) == 0:
+                raise Exception(f'No configured carriers specified')
+
+            request.is_valid(raise_exception=True)
+            response = fetch_rates(request.data, carrier_settings_list)
+
+            return Response(
+                to_dict(response),
+                status=status.HTTP_207_MULTI_STATUS if len(response.messages) > 0 else status.HTTP_200_OK
+            )
+        except Exception as pe:
+            logger.exception(pe)
+            return Response(pe.args, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
+        logger.exception(e)
         return Response(e.args, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
--- purpleserver/proxy/purpleserver/proxy/views/shipping.py
+++ purpleserver/proxy/purpleserver/proxy/views/shipping.py
@@ -1,55 +1,52 @@
+import logging
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from rest_framework import status
 from rest_framework.decorators import api_view, authentication_classes, permission_classes, throttle_classes
 from rest_framework.response import Response
+from rest_framework.request import Request
 from django.urls import path
 
 from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
 
 from purplship.core.utils.helpers import to_dict
 
-from purpleserver.core import serializers
-from purpleserver.core.gateway import fetch_rates
+from purpleserver.core.serializers import ShipmentResponse, ShipmentRequest
+from purpleserver.core.gateway import create_shipment, get_carriers
 from purpleserver.proxy.router import router
+
+logger = logging.getLogger(__name__)
 
 
 @swagger_auto_schema(
     methods=['post'],
-    responses={200: serializers.CompleteShipmentResponse()},
-    request_body=serializers.ShipmentRequest(),
+    request_body=ShipmentRequest(),
+    responses={200: ShipmentResponse()},
     operation_description="""
-    POST /v1/shipping/[carrier?]
+    POST /v1/shipping
     """,
     operation_id="CreateShipment",
-    manual_parameter=[
-        openapi.Parameter(
-            'carrier',
-            openapi.IN_PATH,
-            required=True,
-            description="specific shipping carrier",
-            type=openapi.TYPE_STRING
-        )
-    ]
 )
 @api_view(['POST'])
 @authentication_classes((SessionAuthentication, BasicAuthentication, TokenAuthentication))
 @permission_classes((IsAuthenticated, ))
 @throttle_classes([UserRateThrottle, AnonRateThrottle])
-def rate(carrier: str):
-    error = None
+def ship(request: Request):
     try:
-        if error is None:
-            return Response(
-                to_dict({}),
-                status=status.HTTP_200_OK
-            )
-        else:
-            return Response(error, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            request = ShipmentRequest(data=request.data)
+
+            request.is_valid(raise_exception=True)
+            response = create_shipment(request.data)
+
+            return Response(to_dict(response), status=status.HTTP_201_CREATED)
+        except Exception as pe:
+            logger.exception(pe)
+            return Response(pe.args, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
+        logger.exception(e)
         return Response(e.args, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-router.urls.append(path('shipping/<carrier>', rate, name='Rates'))
+router.urls.append(path('shipping', ship, name='Shipping'))
