e4c917b5644a43cb529a1fc7056734b0c821e95e
DanH91 <danielk.developer@gmail.com>
2021-12-03 10:17:15 -0500

    collapse developer section navigation

--- src/components/sidebars/expanded-sidebar.tsx
+++ src/components/sidebars/expanded-sidebar.tsx
@@ -19,7 +19,7 @@ const ExpandedSidebar: React.FC<ExpandedSidebarComponent> = () => {
   };
   const isActive = (path: string) => {
     if (path === basePath && path === window.location.pathname) return 'is-active';
-    return window.location.pathname === `${basePath}${path}`.replace('//', '/') ? 'is-active' : '';
+    return window.location.pathname.includes(`${basePath}${path}`.replace('//', '/')) ? 'is-active' : '';
   };
 
   return (
@@ -34,65 +34,67 @@ const ExpandedSidebar: React.FC<ExpandedSidebarComponent> = () => {
       </div>
       <div className="sidebar-menu has-slimscroll py-6" style={{ height: "calc(100% - 60px)" }}>
         <AppLink href={basePath} className={"menu-item " + isActive(basePath)}>
-          <span>Shipments</span>
+          <span className="has-text-weight-semibold">Shipments</span>
         </AppLink>
 
         <AppLink href="/trackers" className={"menu-item " + isActive("/trackers")}>
-          <span>Trackers</span>
+          <span className="has-text-weight-semibold">Trackers</span>
         </AppLink>
 
         <AppLink href="/connections" className={"menu-item " + isActive("/connections")}>
-          <span>Carriers</span>
+          <span className="has-text-weight-semibold">Carriers</span>
         </AppLink>
 
         <AppLink href="/templates/addresses" className={"menu-item " + isActive("/templates/addresses")}>
-          <span>Addresses</span>
+          <span className="has-text-weight-semibold">Addresses</span>
         </AppLink>
 
         <AppLink href="/templates/parcels" className={"menu-item " + isActive("/templates/parcels")}>
-          <span>Parcels</span>
+          <span className="has-text-weight-semibold">Parcels</span>
         </AppLink>
 
         <AppLink href="/templates/customs-infos" className={"menu-item " + isActive("/templates/customs-infos")}>
-          <span>Customs</span>
+          <span className="has-text-weight-semibold">Customs</span>
         </AppLink>
 
 
-        <div className="menu-item menu-label my-0">
-          <span>Developers</span>
-        </div>
-
-        <AppLink href="/developers/api" className={"menu-item ml-6 " + isActive("/developers/api")}>
-          <span>API</span>
+        <AppLink href="/developers/api" className="menu-item menu-item my-0">
+          <span className="has-text-weight-semibold">Developers</span>
         </AppLink>
 
-        <AppLink href="/developers/webhooks" className={"menu-item ml-6 " + isActive("/developers/webhooks")}>
-          <span>Webhooks</span>
-        </AppLink>
+        {window.location.pathname.includes('/developers') && <>
+          <AppLink href="/developers/api" className={"menu-item ml-6 " + isActive("/developers/api")}>
+            <span>API</span>
+          </AppLink>
 
-        <AppLink href="/developers/events" className={"menu-item ml-6 " + isActive("/developers/events")}>
-          <span>Events</span>
-        </AppLink>
+          <AppLink href="/developers/webhooks" className={"menu-item ml-6 " + isActive("/developers/webhooks")}>
+            <span>Webhooks</span>
+          </AppLink>
 
-        <AppLink href="/developers/logs" className={"menu-item ml-6 " + isActive("/developers/logs")}>
-          <span>Logs</span>
-        </AppLink>
+          <AppLink href="/developers/events" className={"menu-item ml-6 " + isActive("/developers/events")}>
+            <span>Events</span>
+          </AppLink>
+
+          <AppLink href="/developers/logs" className={"menu-item ml-6 " + isActive("/developers/logs")}>
+            <span>Logs</span>
+          </AppLink>
+        </>}
 
         {testMode ?
           <a className="menu-item mode-menu-item" onClick={switchMode}>
             <i className="fas fa-toggle-on"></i>
-            <span className="mode-menu-item">Viewing test data</span>
+            <span className="mode-menu-item has-text-weight-semibold">Viewing test data</span>
           </a>
           :
           <a className="menu-item has-text-grey" onClick={switchMode}>
             <i className="fas fa-toggle-off"></i>
-            <span>View test data</span>
+            <span className="has-text-weight-semibold">View test data</span>
           </a>
         }
 
         <AppLink href="/settings/account" className={"menu-item " + isActive("/settings/account")}>
           <i className="fas fa-cog"></i>
-          <span>Account Settings</span>
+          <span className="has-text-weight-semibold">Account Settings</span>
         </AppLink>
       </div>
     </div>
