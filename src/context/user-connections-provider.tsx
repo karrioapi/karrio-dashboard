593d15bb73731869d6a91dd4308a3fff25afb07e
DanH91 <danielk.developer@gmail.com>
2022-02-10 13:00:27 -0500

    omit generic carrier query when orders management is not supported

--- src/context/user-connections-provider.tsx
+++ src/context/user-connections-provider.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { LazyQueryResult, useLazyQuery } from '@apollo/client';
-import { get_user_connections, GET_USER_CONNECTIONS, get_user_connections_user_connections } from '@purplship/graphql';
+import { get_user_connections, GET_USER_CONNECTIONS, get_user_connections_user_connections, GET_USER_CONNECTIONS_WITH_GENERICS } from '@purplship/graphql';
+import { APIReference } from './references-provider';
 
 
 export type UserConnectionType = get_user_connections_user_connections;
@@ -13,7 +14,10 @@ type UserConnectionsQueryResult = LazyQueryResult<get_user_connections, any> & {
 export const UserConnections = React.createContext<UserConnectionsQueryResult>({} as UserConnectionsQueryResult);
 
 const UserConnectionsProvider: React.FC = ({ children }) => {
-  const [initialLoad, result] = useLazyQuery<get_user_connections>(GET_USER_CONNECTIONS);
+  const { ORDERS_MANAGEMENT } = useContext(APIReference);
+  const [initialLoad, result] = useLazyQuery<get_user_connections>(
+    ORDERS_MANAGEMENT ? GET_USER_CONNECTIONS_WITH_GENERICS : GET_USER_CONNECTIONS,
+  );
 
   const extract = (results: any[]): UserConnectionType[] => results.filter(r => r !== null);
   const load = (options?: any) => result.called ? result.fetchMore({}) : initialLoad(options);
