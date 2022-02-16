bcecc86e003589c3867888cf0b0069ac1f78a27f
DanH91 <danielk.developer@gmail.com>
2022-02-15 19:56:02 -0500

    allow responsiveness on mobile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import 'highlight.js/styles/stackoverflow-light.css';
 import '@/styles/theme.scss';
 import '@/styles/dashboard.scss';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { SessionProvider } from "next-auth/react";
 import MainLayout from '@/layouts/main-layout';
 import { ClientsProvider } from '@/client/context';
@@ -12,9 +11,6 @@ import { ClientsProvider } from '@/client/context';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session} refetchInterval={5 * 60}>
-      <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
-      </Head>
       <ClientsProvider>
         <MainLayout error={pageProps?.error}>
           <Component {...pageProps} />
