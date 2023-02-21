e9794d3cd58ac3b5e36054434d4e18840691a368
Daniel K <danielk.developer@gmail.com>
2023-02-21 03:53:58 -0800

    (fix) tracking page request with valid tenant host url

--- src/lib/data-fetching/tracker.ts
+++ src/lib/data-fetching/tracker.ts
@@ -1,16 +1,18 @@
 import { loadAPIMetadata } from "@/lib/data-fetching";
+import { KarrioClient } from "@karrio/rest";
+import { KARRIO_API } from "@/lib/client";
 import { GetServerSideProps } from "next";
-import { rest$ } from "@/lib/client";
 
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { res, params } = ctx;
   const id = params?.id as string;
   const metadata = await loadAPIMetadata(ctx).catch(_ => _);
+  const client = new KarrioClient({ basePath: `${metadata.metadata?.HOST || KARRIO_API}` });
 
   try {
     // Retrieve tracker by id
-    const data = await rest$.value?.trackers.retrieves({ idOrTrackingNumber: id })
+    const data = await client.trackers.retrieves({ idOrTrackingNumber: id })
       .then(({ data }) => ({ tracker: JSON.parse(JSON.stringify(data)) }))
       .catch(_ => ({ message: `No Tracker ID nor Tracking Number found for ${id}` }));
 
